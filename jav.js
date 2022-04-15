e81b22eaa1b96ca62b73d6ed6610806bb7cf8e5e
robe;l <robelalam3421@gmail.com>
2022-04-15 21:25:53 +0400

    second commit, having some problems implementing additional functions.

--- jav.js
+++ jav.js
@@ -1,23 +1,64 @@
 const cDiv = document.querySelector('.container');
 
-  for (let i = 1; i < 257; i++) {
+const grids = document.querySelector('#gridSize')
+
+const resetButton = document.querySelector('#reset')
+
+
+window.onload = gridMake();
+
+
+function gridMake() {
+  for (let i = 1; i < 1250; i++) {
     const div = document.createElement('div');
-    cdiv.appendChild(div);
+    cDiv.appendChild(div);
     div.addEventListener('mouseover', () =>{
-      div.style.backgroundColor = 'red';
+      div.style.backgroundColor = randomColor();  
     })
-
+    div.addEventListener('mouseover', () =>{
+     div.style.backgroundClip = changeColor;
+    })
+    grids.addEventListener('click', () => {
+      cDiv.removeChild(div)
+      
+    })
+   
   }
+};
+  
+  function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
 
-
- const grids = document.querySelector('button');
+ 
  grids.addEventListener('click', function(){
    gridSize()
  })
 
 function gridSize () {
   let i = prompt('how many grids do you want?',);
-  for ( ; i <= 100; i++) {
-    const div = document.createElement('div');
-    cdiv.appendChild(div);
+  if( i < 200){ 
+    return alert('The number exceeds the limit')
+   }  
+   else{
+    for (let j = 0;j <= i; j++) {
+      const div = document.createElement('div');
+
+      cDiv.appendChild(div);
+      grids.setAttribute('disabled', true)
+      div.addEventListener('mouseover', () =>{
+        div.style.backgroundColor = randomColor();  
+      })
+     
+   }
+
+ 
 }}
+
+resetButton.addEventListener('click', () =>{
+  window.location.reload()
+});
